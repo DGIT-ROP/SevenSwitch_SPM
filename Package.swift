d49088e3105d7bf227d98e4df2980c109ec34234
DGIT-ROP <dgit.rop@gmail.com>
2024-08-08 14:27:30 +0400

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SevenSwitch",
+    products: [
+        .library(
+            name: "SevenSwitch",
+            targets: ["SevenSwitch"]),
+    ],
+    targets: [
+        .target(
+            name: "SevenSwitch",
+            dependencies: [])
+    ]
+)
